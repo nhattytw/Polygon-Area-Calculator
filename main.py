586df454229260115ae5a8477f1fa35f2acb7a8d
nhattytw <nhatty567@outlook.com>
2021-12-10 21:43:52 +0300

    modified

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import shape_calculator
 from unittest import main
 
 
-rect = shape_calculator.Rectangle(5, 10)
+rect = shape_calculator.Rectangle(10, 5)
 print(rect.get_area())
-rect.set_width(5)
+rect.set_height(3)
 print(rect.get_perimeter())
 print(rect)
 
-sq = shape_calculator.Square(7)
+sq = shape_calculator.Square(9)
 print(sq.get_area())
 sq.set_side(4)
 print(sq.get_diagonal())
